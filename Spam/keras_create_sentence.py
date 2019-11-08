1e247abf8d06dd1a0b536bc642c406db1132f0ba
Ryo <ryo@nsl.cs.waseda.ac.jp>
2019-11-08 21:49:17 +0900

    fixed_name

--- Spam/keras_create_sentence.py
+++ Spam/keras_create_sentence.py
@@ -72,7 +72,7 @@ for iteration in range(1, 60):
     print('Iteration', iteration)
     model.fit(X, y,
               batch_size=128,
-              epochs=1)
+              epochs=5)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
 
